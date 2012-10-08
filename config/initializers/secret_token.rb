4ff061d33d078d9925ec174d40b08a56b6cd6483
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2012-10-08 13:30:26 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ceshi::Application.config.secret_token = '86f35f0d351f6f597dc9854fb38cd809c1c8d468667740ce0a9814948641abb4d6b004b9c63deed8127364c3559edc8d58f381982e07cef8f6ecb4c4a0efc91b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ceshi::Application.config.session_store :cookie_store, key: '_ceshi_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ceshi::Application.config.session_store :active_record_store
